3527c1e77a50a3903d1e6f49bdf0ace786b66324
pfeldman <pablofeldman90@gmail.com>
2011-05-31 21:53:28 -0300

    Agrego Tabs al editor

--- devEditor.1.py
+++ devEditor.1.py
@@ -4,17 +4,36 @@ import os
 import re
 
 global pathName
-
+global activaTab
+global activeFiles
+global textC
 class MainWindow(wx.Frame):
   def __init__(self, parent, title):
-    wx.Frame.__init__(self, parent, title=title, size=(500,500))
+    global activaTab
+    global activeFiles
+    global textC
+    
+    textC = []
+    wx.Frame.__init__(self, parent, title=title, size=(1000,1000))
+    self.SetIcon(wx.Icon('ico.ico', wx.BITMAP_TYPE_ICO))
     grilla = wx.GridBagSizer()
     self.treeView = wx.TreeCtrl(self)
+    self.panel = []
+    grilla.Add(self.treeView,(0,0),(40,20), wx.EXPAND)
     
-    grilla.Add(self.treeView,(0,0),(10,20), wx.EXPAND)
     
-    self.textCtrl = wx.TextCtrl(self, style= (wx.TE_MULTILINE | wx.TE_DONTWRAP))
-    grilla.Add(self.textCtrl,(0,20),(10,26), wx.EXPAND)
+    #grilla.Add(self.textCtrl,(1.5,20),(40,76), wx.EXPAND)
+    
+    self.tabbed = wx.Notebook(self, -1, style=(wx.NB_TOP))
+    self.panel.append(wx.NotebookPage(self.tabbed, -1))
+    self.tabbed.AddPage(self.panel[0], "Untitled")
+    activaTab = 0
+    activeFiles = []
+    activeFiles.append("")
+    textC.append(wx.TextCtrl(self.panel[0], style= (wx.TE_MULTILINE | wx.TE_DONTWRAP)))
+    
+    self.tabbed.Bind(wx.EVT_NOTEBOOK_PAGE_CHANGED, self.OnPageChanged)
+    grilla.Add(self.tabbed,(0,20),(39,76), wx.EXPAND)
     grilla.AddGrowableCol(20)
     grilla.AddGrowableRow(2)
     
@@ -22,7 +41,6 @@ class MainWindow(wx.Frame):
     self.CreateStatusBar()
 
     filemenu= wx.Menu()
-
     menuOpen = filemenu.Append(wx.ID_OPEN, "&Open"," Open a new file")
     menuOpenProy = filemenu.Append(0, "&Open Proyect"," Open a full preyect file")
     filemenu.AppendSeparator()
@@ -34,10 +52,10 @@ class MainWindow(wx.Frame):
     menuBar = wx.MenuBar()
     menuBar.Append(filemenu,"&File")
     self.SetMenuBar(menuBar)
-    self.textCtrl.SetFont(wx.Font(10, wx.FONTFAMILY_TELETYPE, wx.NORMAL, wx.FONTWEIGHT_NORMAL))
+    textC[0].SetFont(wx.Font(10, wx.FONTFAMILY_TELETYPE, wx.NORMAL, wx.FONTWEIGHT_NORMAL))
     
     self.Bind(wx.EVT_TREE_ITEM_ACTIVATED, self.OnActivated, self.treeView)
-    self.textCtrl.Bind(wx.EVT_KEY_UP, self.hightlighting)
+    textC[0].Bind(wx.EVT_KEY_UP, self.hightlighting)
     self.Bind(wx.EVT_MENU, self.OnOpenProyectFolder, menuOpenProy)
     self.Bind(wx.EVT_MENU, self.OnOpenFile, menuOpen)
     self.Bind(wx.EVT_MENU, self.OnSaveFile, menuSave)
@@ -45,9 +63,37 @@ class MainWindow(wx.Frame):
     self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
 
     self.Show(True)
-  def OnActivated(self, evt):
-    print "OnActivated:    ", self.GetItemText(evt.GetItem())
-  
+    
+  def OnPageChanged(self, e):
+    activaTab = e.GetSelection()
+    self.hightlighting(e)
+    
+  def OnActivated(self, e):
+    global textC
+    global activeFiles
+    realPathName = self.GetItemText(e.GetItem()).split("  				-  \"")[1][:-1]
+    filename = realPathName = self.GetItemText(e.GetItem()).split("  				-  \"")[0]
+    continueCon = True
+    item = 0
+    for i in range(0, len(activeFiles)):
+      if activeFiles[i] == realPathName:
+	continueCon = False
+	item = i
+	self.tabbed.SetSelection(item)
+	
+    if continueCon == True:
+      self.panel.append(wx.NotebookPage(self.tabbed, -1))
+      self.tabbed.AddPage(self.panel[len(self.panel)-1], filename)
+      textC.append(wx.TextCtrl(self.panel[len(self.panel)-1], style= (wx.TE_MULTILINE | wx.TE_DONTWRAP)))
+      activeFiles.append(realPathName)
+      f = open(realPathName, 'r')
+      textC[len(textC)-1].SetValue(f.read())
+      f.close()
+      textC[len(textC)-1].SetFont(wx.Font(10, wx.FONTFAMILY_TELETYPE, wx.NORMAL, wx.FONTWEIGHT_NORMAL))
+      textC[len(textC)-1].Bind(wx.EVT_KEY_UP, self.hightlighting)
+      self.tabbed.SetSelection(len(textC)-1)
+      self.hightlighting(e)
+
   def GetItemText(self, item):
     if item:
       return self.treeView.GetItemText(item)
@@ -65,7 +111,6 @@ class MainWindow(wx.Frame):
   def OnOpenProyectFolder(self,e):
     dialog = wx.DirDialog(None, "Choose a directory:",style=wx.DD_DEFAULT_STYLE | wx.DD_NEW_DIR_BUTTON)
     if dialog.ShowModal() == wx.ID_OK:
-      #a = os.system("sabelo[0]='/home/pablo/Fuentes/deveditor/devEditor.old.py'")
       a = os.system('find ' + dialog.GetPath() + ' > ../foundedDirs.txt')
       f = open('../foundedDirs.txt', 'r')
       a = f.readline()
@@ -87,50 +132,72 @@ class MainWindow(wx.Frame):
 	  if len(c) > lastlen:
 	    lastlen = len(c)
 	    if lastli + 1>=len(li):
-	      li.append(self.treeView.AppendItem(li[len(li)-1], c[lastlen - 1]))
+	      li.append(self.treeView.AppendItem(li[len(li)-1], c[lastlen - 1] + "  				-  \"" + dialog.GetPath() + b[1] + "\""))
 	      lastli = len(li)-1
 	    else:
-	      li[lastli + 1] = self.treeView.AppendItem(li[lastli], c[lastlen - 1])
+	      li[lastli + 1] = self.treeView.AppendItem(li[lastli], c[lastlen - 1] + "  				-  \"" + dialog.GetPath() + b[1] + "\"")
 	      lastli = lastli+1
 	    parent = lastli - 1
 	  elif len(c) == lastlen:
-	    li[lastli] = self.treeView.AppendItem(li[parent], c[lastlen - 1])
+	    li[lastli] = self.treeView.AppendItem(li[parent], c[lastlen - 1] + "  				-  \"" + dialog.GetPath() + b[1] + "\"")
 	  elif len(c) < lastlen:
 	    dif = lastlen - len(c)
 	    lastli = len(c)-1
 	    parent = lastli - 1
 	    lastlen = len(c)
-	    li[lastli] = self.treeView.AppendItem(li[parent], c[lastlen - 1])
+	    li[lastli] = self.treeView.AppendItem(li[parent], c[lastlen - 1] + "  				-  \"" + dialog.GetPath() + b[1] + "\"")
 	  
       os.system('rm ../foundedDirs.txt')
       dialog.Destroy()
       
   def OnOpenFile(self,e):
     global pathName
+    global activeFiles
+    global textC
     self.dirname = ''
-    dlg = wx.FileDialog(self, "Open file", self.dirname, "", "*.*;*.txt", wx.OPEN)
+    dlg = wx.FileDialog(self, "Open file", self.dirname, "", "*", wx.OPEN)
     if dlg.ShowModal() == wx.ID_OK:
       filename = dlg.GetFilename()
-      self.Title = filename + " - DevEditor"
       self.dirname = dlg.GetDirectory()
       pathName = os.path.join(self.dirname, filename)
-      f = open(pathName, 'r')
-      self.textCtrl.SetValue(f.read())
-      f.close()
+      continueCon = True
+      for i in range(0, len(activeFiles)):
+	if activeFiles[i] == pathName:
+	  item = i
+	  self.tabbed.SetSelection(item)
+	  continueCon = False
+	  
+      if continueCon == True:
+	self.panel.append(wx.NotebookPage(self.tabbed, -1))
+	self.tabbed.AddPage(self.panel[len(self.panel)-1], filename)
+	textC.append(wx.TextCtrl(self.panel[len(self.panel)-1], style= (wx.TE_MULTILINE | wx.TE_DONTWRAP)))
+	activeFiles.append(pathName)
+	f = open(pathName, 'r')
+	self.tabbed.SetSelection(len(textC)-1)
+	textC[len(textC)-1].SetValue(f.read())
+	textC[len(textC)-1].SetFont(wx.Font(10, wx.FONTFAMILY_TELETYPE, wx.NORMAL, wx.FONTWEIGHT_NORMAL))
+	textC[len(textC)-1].Bind(wx.EVT_KEY_UP, self.hightlighting)
+	f.close()
+	self.hightlighting(e)
       dlg.Destroy()
-      self.hightlighting(e)
+      
       
   def hightlighting(self, e):
-    text = self.textCtrl.GetValue()
+    indexText = self.tabbed.GetSelection()
+    global textC
+    text = textC[indexText].GetValue()
+    textC[indexText].SetStyle(0, len(textC[indexText].GetValue()) ,wx.TextAttr('#000000', wx.NullColour , wx.Font(10, wx.FONTFAMILY_TELETYPE, wx.NORMAL, wx.FONTWEIGHT_NORMAL)))
     search = ["function",
 	      "/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+/", 
 	      "\"([^\"]+)\"",
-	      "\'([^\']+)\'"]
+	      "\'([^\']+)\'",
+	      "//([^\n]+)\n"]
 	      
     searchCol = [wx.TextAttr('#000000', wx.NullColour , wx.Font(10, wx.FONTFAMILY_TELETYPE, wx.NORMAL, wx.FONTWEIGHT_BOLD)), 
 		 wx.TextAttr('#A4A4A4', wx.NullColour , wx.Font(10, wx.FONTFAMILY_TELETYPE, wx.ITALIC, wx.FONTWEIGHT_LIGHT)),
 		 wx.TextAttr('#B40404', wx.NullColour , wx.Font(10, wx.FONTFAMILY_TELETYPE, wx.NORMAL, wx.FONTWEIGHT_LIGHT)),
-		 wx.TextAttr('#B40404', wx.NullColour , wx.Font(10, wx.FONTFAMILY_TELETYPE, wx.NORMAL, wx.FONTWEIGHT_LIGHT))]
+		 wx.TextAttr('#B40404', wx.NullColour , wx.Font(10, wx.FONTFAMILY_TELETYPE, wx.NORMAL, wx.FONTWEIGHT_LIGHT)),
+		 wx.TextAttr('#A4A4A4', wx.NullColour , wx.Font(10, wx.FONTFAMILY_TELETYPE, wx.ITALIC, wx.FONTWEIGHT_LIGHT))]
     ind = 0
     lessWord = ""
     lessInd = 0
@@ -144,12 +211,12 @@ class MainWindow(wx.Frame):
 	  if (lessInd <> None):
 	    lessWord = text[lessInd.start(): lessInd.end()]
 	    index = i
-	elif p.search(text) != None and p.search(text).start() < lessInd.start():
-	  lessInd = p.search(text)
-	  lessWord = text[lessInd.start():lessInd.end()]
-	  a = lessInd
-	  lessInd = lessInd.start()
-	  index = i
+	elif p.search(text) != None:
+	  if p.search(text).start() < lessInd.start():
+	    lessInd = p.search(text)
+	    lessWord = text[lessInd.start():lessInd.end()]
+	    a = lessInd
+	    index = i
       
       ind = text.find(lessWord)
       ind = ind + plus
@@ -157,33 +224,37 @@ class MainWindow(wx.Frame):
 	ind = -1
 	
       if (ind <> -1):
-	self.textCtrl.SetStyle(long(ind), long(ind + len(lessWord)),searchCol[index])
+	textC[indexText].SetStyle(long(ind), long(ind + len(lessWord)),searchCol[index])
 	ind = ind - plus
 	text = text[ind + len(lessWord):]
 	plus = plus + ind + len(lessWord)
-	lessWord = ""
-	
-	
-	
-      
+	lessWord = ""      
       
   def OnSaveFile(self,e):
-    global pathName
-    if pathName <> '':
+    global activeFiles
+    global textC
+    indexText = self.tabbed.GetSelection()
+    if activeFiles[indexText] <> '':
       f = open(pathName, 'w')
-      f.write(self.textCtrl.GetValue())
+      f.write(textC[indexText].GetValue())
+      f.close()
     else:
       self.OnSaveAsFile(e)
 
   def OnSaveAsFile(self,e):
+    global activeFiles
+    global textC
+    indexText = self.tabbed.GetSelection()
     self.dirname = ''
-    dlg = wx.FileDialog(self, "Save As", self.dirname, "", "*.*;*.txt", wx.SAVE)
+    dlg = wx.FileDialog(self, "Save As", self.dirname, "", "*.*", wx.SAVE)
     if dlg.ShowModal() == wx.ID_OK:
       filename = dlg.GetFilename()
       self.dirname = dlg.GetDirectory()
       pathName = os.path.join(self.dirname, filename)
+      self.tabbed.SetPageText(indexText,filename) 
       f = open(pathName, 'w')
-      f.write(self.textCtrl.GetValue())
+      activeFiles[indexText] = pathName
+      f.write(textC[indexText].GetValue())
       f.close()
       dlg.Destroy()
       
